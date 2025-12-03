2098ceaf644ffe728d53dc538aee94bc3bac897f
FrancAdam <Adam2.Franc@live.uwe.ac.uk>
2025-12-03 13:21:45 +0000

    added more passports and animals

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -8,9 +8,15 @@ Animal::~Animal()
 bool Animal::initAnimalTextures()
 {
 	animal_texture_location = {
-				"../data/Critter Crossing Customs/elephant.png",
-				"../data/Critter Crossing Customs/moose.png",
-				"../data/Critter Crossing Customs/penguin.png"};
+				"../data/Critter Crossing Customs/animals/elephant.png",
+				"../data/Critter Crossing Customs/animals/moose.png",
+				"../data/Critter Crossing Customs/animals/penguin.png",
+				"../data/Critter Crossing Customs/animals/chicken.png",
+				"../data/Critter Crossing Customs/animals/buffalo.png",
+				"../data/Critter Crossing Customs/animals/gorilla.png",
+				"../data/Critter Crossing Customs/animals/narwhal.png",
+				"../data/Critter Crossing Customs/animals/walrus.png",
+				"../data/Critter Crossing Customs/animals/giraffe.png"};
 
 	initTextures(animal_textures, animal_texture_location);
 	
@@ -33,23 +39,3 @@ int Animal::getAnimalSize()
 	return animal_texture_location.size();
 }
 
-//void Animal::changeAnimal()
-//{
-//    int max_animals = animal_texture_location.size();
-//    int random_index = getRandInt(0, max_animals - 1);
-//    std::cout << "Selected animal index: " << random_index << std::endl;
-//
-//
-//    // Set the texture and reset the texture rectangle
-//    sprite->setTexture(*animal_textures[random_index], true);
-//
-//    // Reset previous scale
-//    sprite->setScale(1.f, 1.f);
-//
-//    // Optional: scale uniformly to fit within a max size
-//    const float maxSize = 100.f; // max width or height you want on screen
-//    sf::Vector2u texSize = animal_textures[random_index]->getSize();
-//    float scaleFactor = std::min(maxSize / static_cast<float>(texSize.x),
-//        maxSize / static_cast<float>(texSize.y));
-//    sprite->setScale(scaleFactor, scaleFactor);
-//}
--- src/Passport.cpp
+++ src/Passport.cpp
@@ -8,9 +8,15 @@ Passport::~Passport()
 bool Passport::initPassportTextures()
 {
 	passport_texture_location = {
-			"../data/Critter Crossing Customs/elephant passport.png",
-			"../data/Critter Crossing Customs/moose passport.png",
-			"../data/Critter Crossing Customs/penguin passport.png" };
+				"../data/Critter Crossing Customs/passports/elephant_passport.png",
+				"../data/Critter Crossing Customs/passports/moose_passport.png",
+				"../data/Critter Crossing Customs/passports/penguin_passport.png",
+				"../data/Critter Crossing Customs/passports/chicken_passport.png",
+				"../data/Critter Crossing Customs/passports/buffalo_passport.png",
+				"../data/Critter Crossing Customs/passports/gorilla_passport.png",
+				"../data/Critter Crossing Customs/passports/narwhal_passport.png",
+				"../data/Critter Crossing Customs/passports/walrus_passport.png",
+				"../data/Critter Crossing Customs/passports/giraffe_passport.png" };
 
 	initTextures(passport_textures, passport_texture_location);
 
